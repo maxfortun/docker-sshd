8aca303996376bdb383cabcb9e9d36ba095e01c0
Max <max@maxf.net>
2020-09-06 10:59:26 -0400

    Adding latest

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ popd
 
 . $SWD/setenv.sh
 
-docker build ${DOCKER_BUILD_ARGS[*]} ${LAST_LAYER_ARGS[*]} --rm -t "$REPO/$NAME:$VERSION" $NAME
+docker build ${DOCKER_BUILD_ARGS[*]} ${LAST_LAYER_ARGS[*]} --rm -t "$REPO/$NAME:$VERSION" -t "$REPO/$NAME:latest" $NAME
 
 dockerImages=$(docker images "$REPO/$NAME" -f "before=$REPO/$NAME:$VERSION" -q)
 [ -n "$dockerImages" ] && docker rmi -f $dockerImages || true
